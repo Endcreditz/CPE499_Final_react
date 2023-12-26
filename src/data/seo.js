42e6f246dc1897a7198607e56adb8ca8a1a5ba55
End_creditz <End_creditz@hotmail.com>
2023-12-26 14:10:28 +0700

    first commit

---
+++ src/data/seo.js
@@ -0,0 +1,17 @@
+const SEO = [
+	{
+		page: "home",
+		description:
+			"",
+		keywords: ["Kritsakorn", "Kritsakorn C", "Kritsakorn Chansiri"],
+	},
+
+	{
+		page: "about",
+		description:
+			"",
+		keywords: ["Kritsakorn", "Kritsakorn C", "Kritsakorn Chansiri"],
+	}
+];
+
+export default SEO;
---
+++ src/data/user.js
@@ -0,0 +1,34 @@
+const INFO = {
+	main: {
+		title: "Reactfolio by truethari",
+		name: "Kritsakorn C.",
+		email: "Kritsakorn.c63@rsu.ac.th",
+		logo: "../logo.png",
+	},
+
+	socials: {
+		twitter: "https://twitter.com/",
+		github: "https://github.com/",
+		linkedin: "https://linkedin.com/",
+		instagram: "https://instagram.com/",
+		stackoverflow: "https://stackoverflow.com/",
+		facebook: "https://facebook.com/",
+	},
+
+	homepage: {
+		title: "Rangsit University : Student",
+		description:
+			"สวัสดีครับยินดีต้อนรับสู่ Web ของผมครับ โดยผมได้นำ template มาจาก Reactfolio by truethari แล้วนำมาใช้เรียนรู้ในการทำ Route ทั้งในส่วน Home , About และหน้า Error ถ้าต้องการไปหน้า Error ให้แก้ไข url ที่หน้า about เป็นคำอื่นๆแทน ",
+	},
+
+	about: {
+		title: "สวัสดีครับ ผม ชื่อ กฤษกร จันทร์ศิริ ยินดีที่รู้จักครับ",
+		description:
+			"ผมมีชื่อเล่น ว่า แม็ก อายุ 21 ปี ครับ อาหารที่ชอบคือ ยำ และ ผัดไท ครับ ชอบฟังแนวเพลง คือ เพลง Rock ครับ ",
+	},
+
+	
+	
+};
+
+export default INFO;
