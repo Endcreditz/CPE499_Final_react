d4ad38cd04d129c97e30b72cddbec0526eef39e5
End_creditz <End_creditz@hotmail.com>
2023-12-26 15:28:18 +0700

    Final

--- src/App.js
+++ src/App.js
@@ -1,5 +1,5 @@
 import { useEffect } from "react";
-import { Routes, Route } from "react-router-dom";
+import { Routes, Route, BrowserRouter } from "react-router-dom";
 import ReactGA from "react-ga4";
 
 import Homepage from "./pages/homepage";
